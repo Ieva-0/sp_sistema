68b2af3f0e33c5a66f3b8c27471520e52a591e1a
Cechanavičius Mantas <mancec1@ktu.lt>
2019-11-11 15:07:37 +0200

    Added page layouts for student

--- routes/web.php
+++ routes/web.php
@@ -35,3 +35,46 @@ Route::get('/imone/redaguoti_paskaita', function () {
 Route::get('/imone/redaguoti_paskaita/paskaita1', function () {
     return view('Imone/paskaita1');
 });
+
+
+
+
+Route::get('/studentas', function () {
+    return view('Studentas/studento_pagrindinis_meniu');
+});
+
+Route::get('/studentas/prisijungti', function () {
+    return view('Studentas/studento_prisijungimas');
+});
+
+Route::get('/studentas/uzsiemimu_registracija', function () {
+    return view('Studentas/studento_uzsiemimu_registracija');
+});
+
+Route::get('/studentas/Erasmus', function () {
+    return view('Studentas/studento_Erasmus_registracija');
+});
+
+Route::get('/studentas/karjeros_mentorius', function () {
+    return view('Studentas/studento_karjeros_mentorio_registracija');
+});
+
+Route::get('/studentas/mokslo_grupes', function () {
+    return view('Studentas/studento_mokslo_grupes_registracija');
+});
+
+Route::get('/studentas/praktikos', function () {
+    return view('Studentas/studento_praktikos_registracija');
+});
+
+Route::get('/studentas/profilis', function () {
+    return view('Studentas/studento_profilio_perziura');
+});
+
+Route::get('/studentas/profilio_redagavimas', function () {
+    return view('Studentas/studento_profilio_redagavimas');
+});
+
+Route::get('/studentas/tvarkarastis', function () {
+    return view('Studentas/studento_tvarkarastis');
+});
