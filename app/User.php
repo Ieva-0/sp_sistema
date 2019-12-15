90a38750cbbfab95e841f0d0b73645c149e3bb2b
Ieva-0 <lukoseviciute.ie@gmail.com>
2019-12-15 16:32:05 +0200

    registracija, prisijungimas
    registruojantis automatiskai sukuriamas irasas studento lenteleje

--- app/User.php
+++ app/User.php
@@ -15,7 +15,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'name', 'email', 'password', 'user_level'
     ];
     /**
      * The attributes that should be hidden for arrays.
@@ -25,4 +25,9 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+    public function setPasswordAttribute($password)
+    {
+        $this->attributes['password'] = bcrypt($password);
+    }
 }
